92408ce94995436b3465feb885b4ccd7594e4163
Atia Khan <atia.khan@xloopdigital.com>
2023-07-10 12:36:42 +0500

    database data has been added

--- src/main/java/com/atia/existourist/Model/Hotel.java
+++ src/main/java/com/atia/existourist/Model/Hotel.java
@@ -1,5 +1,6 @@
 package com.atia.existourist.Model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,13 +21,16 @@ public class Hotel {
 @Id
 @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+    
     private String imgLink;
     private String hotelName;
     private String location;
     private boolean isPool;
     private String experienceLevel;
     private double pricePerNight;
+  
     private String description;
+@Column(length = 1000)
     private String longDesc;
 
     
